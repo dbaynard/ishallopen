a61d56ebbb103640dd7352139543b3a203b50b48
David Baynard <davidbaynard@gmail.com>
2016-07-02 17:57:58 +0100

    Add tests

--- server/test/HallDatesSpec.hs
+++ server/test/HallDatesSpec.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE ViewPatterns #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE LambdaCase #-}
 
 module HallDatesSpec (
     module HallDatesSpec
@@ -12,25 +14,49 @@ import Control.Exception (evaluate)
 import Control.Monad
 import Control.Arrow
 import Data.Coerce
+import Data.Maybe (fromMaybe)
+
+import Data.Thyme
+import Data.Thyme.Calendar.WeekDate
+import System.Locale
 
 import HallDates
-import Data.Thyme
-import System.Locale
+import Lens
 
 main :: IO ()
 main = hspec spec
 
 spec :: Spec
-spec = do
+spec =
     describe "Hall open status" $ do
-        it "Is closed between August 12th and 30th inclusive" $ do
-            property $ \(x :: ClosedDates) -> (ishallopentoday . coerce) x == NoHallForAges
+        it "is closed between August 12th and 30th inclusive" $
+            property $ \(x :: ClosedDates) -> (ishallopentoday . coerce) x === NoHallForAges
+        it "is closed on other Saturdays before 29th September" $
+            property $ \(x :: SummerDates) -> (dayOfWeek . coerce) x == 6 ==> (ishallopentoday . coerce) x === NoHallToday
+        it "is open for dinner on other Sundays before 29th September" $
+            property $ \(x :: SummerDates) -> (dayOfWeek . coerce) x == 7 ==> (ishallopentoday . coerce) x === DinnerToday
+        it "reads MCR dinner when there are MCR dinners" $
+            ishallopentoday (read "2016-07-12") === MCRDinnerToday
+        it "is open for breakfast and lunch on other weekdays before 29th September" $
+            property $ \(x :: SummerDates) ->
+                    (\z@(dayOfWeek . coerce -> y) -> coerce z /= (read "2016-07-12" :: Day) && y >= 1 && y <= 5) x
+                    ==> (ishallopentoday . coerce) x === BandLToday
 
 newtype ClosedDates = ClosedDates Day
+    deriving (Show)
+newtype SummerDates = SummerDates Day 
     deriving (Show)
 
 instance Arbitrary ClosedDates where
    arbitrary = "2016-08-12" `dateRange` "2016-08-30"
+instance Arbitrary SummerDates where
+   arbitrary = oneof [
+                       "2016-06-25" `dateRange` "2016-08-11"
+                     , "2016-08-31" `dateRange` "2016-09-28"
+                     ]
+
+dayOfWeek :: Day -> DayOfWeek
+dayOfWeek = view (weekDate . _wdDay)
 
 dateRange :: Coercible Day a
           => String -- ^ From date
@@ -43,7 +69,7 @@ parseDay :: String -> Maybe Day
 parseDay = parseTime ukTimeLocale "%Y-%m-%d"
 
 readDay :: String -> Day
-readDay = maybe (error "Can’t read day") id . parseDay
+readDay = fromMaybe (error "Can’t read day") . parseDay
 
 mzip :: Monad m => m a -> m b -> m (a, b)
 mzip = curry $ runKleisli (Kleisli id *** Kleisli id)
