b77dbf881bc13a5ae5fdd3e6bf17d4deeee1d4a9
David Baynard <davidbaynard@gmail.com>
2016-07-14 01:59:22 +0100

    Annotate deployment code

--- deploy/src/Build.hs
+++ deploy/src/Build.hs
@@ -1,3 +1,15 @@
+{-|
+Module      : Build
+Description : Deploy website
+Maintainer  : David Baynard <davidbaynard@gmail.com>
+
+Copyright   : David Baynard 2016
+License     : Apache
+
+This uses <http://shakebuild.com/ Shake> to build the website.
+
+-}
+
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE PartialTypeSignatures #-}
@@ -5,7 +17,7 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
 module Build (
-    module Build
+    build
 )   where
 
 import BasicPrelude
@@ -34,31 +46,58 @@ newtype GhcjsFolder = GhcjsFolder ()
 data Flag = Development
     deriving (Eq, Ord, Show, Read, Enum, Bounded)
 
+{-|
+  Flags to be passed to the 'build' function.
+-}
 cmdFlags :: [OptDescr (Either String Flag)]
 cmdFlags = [Option "D" ["development"] (NoArg . Right $ Development) "Development mode — do not compile javascript"]
 
+{-|
+  This is where the magic happens.
+
+  'shakeArgsWith' uses 'cmdFlags' to toggle development mode (i.e. whether the javascript is minified).
+  
+  The 'want' function adds the 'phony' `clean` target to 'shakeArgsWith'. 
+
+  The minified output javascript is only a dependency if development mode is off.
+
+  Use the argument `clean` to clear up after compilation.
+-}
 build :: IO ()
 build = shakeArgsWith shakeOptions{shakeFiles=shakeDir} cmdFlags $ \flags targets -> pure . pure $ do
     want $ [ ishallopentodayHtml 
            , outputJS
            , distDir </> "ishallopentoday" <.> "js"
            , distDir </> index
-           ] <> targets
+           ] <> targets -- Adds `clean` target
+    -- | Only want outputMinJS if not in development mode.
     action $
         when (Development `notElem` flags) $ need [outputMinJS]
+    -- | Get information about compilers, therefore about stack directory
+    -- structure
     addOracle' getGhcVersion
     addOracle' getGhcjsVersion
     addOracle' getGhcjsFolder
+    -- | Clear up
     "clean" ~> clean
+    -- | The heavy lifting
     ishallopentodayHtml %> getGen
     outputJS %> genJS
     outputMinJS %> genMinJS
     distDir </> "ishallopentoday" <.> "js" %> if Development `elem` flags then getJS else getMinJS
     distDir </> index %> getHtml
 
+{-|
+  This helper function ignores the return value for 'addOracle'.
+-}
 addOracle' :: (ShakeValue q, ShakeValue a) => (q -> Action a) -> Rules ()
 addOracle' = void . addOracle
 
+{-|
+  Clear up afterwards!
+
+  This has to be fixed manually after any changes.
+-}
 clean :: Action ()
 clean = do
         putNormal $ "Removing files in " <> ", " `intercalate` [shakeDir, buildDir] <> "and removing " <> ishallopentodayHtml
@@ -66,6 +105,9 @@ clean = do
         removeFilesAfter "." [ishallopentodayHtml]
         removeFilesAfter shakeDir ["//*"]
 
+{-|
+  Produce the `client.js` file.
+-}
 genJS :: FilePath -> Action ()
 genJS out = do
         ghcjs <- askOracle (GhcjsVersion ())
@@ -81,30 +123,45 @@ genJS out = do
         command_ [] "sed" ["-i", "s/goog.provide.*//", out]
         command_ [] "sed" ["-i", "s/goog.require.*//", out]
 
+{-|
+  Copy the `client.js` file to the distribution (gh-pages) directory.
+-}
 getJS :: FilePath -> Action ()
 getJS out = do
         alwaysRerun
         need [outputJS]
         outputJS `copyFile'` out
 
+{-|
+  Produce the `client.min.js` file.
+-}
 genMinJS :: FilePath -> Action ()
 genMinJS out = do
         need [outputJS]
         Stdout minOut <- command [] "closure" ["--compilation_level=ADVANCED_OPTIMIZATIONS", outputJS] :: Action (Stdout ByteString)
         liftIO . B.writeFile out $ minOut
 
+{-|
+  Copy the `client.min.js` file to the distribution (gh-pages) directory.
+-}
 getMinJS :: FilePath -> Action ()
 getMinJS out = do
         alwaysRerun
         need [outputMinJS]
         outputMinJS `copyFile'` out
 
+{-|
+  Produce the `index.html` file in the distribution directory.
+-}
 getHtml :: String -> Action ()
 getHtml out = do
         need [ishallopentodayHtml]
         Stdout html <- command [] "./ishallopentoday-html" []
         liftIO . B.writeFile out $ html
 
+{-|
+  Compile the executable that will produce the html for the site.
+-}
 getGen :: String -> Action ()
 getGen out = do
         command_ [] "stack" ["build", out]
@@ -113,9 +170,15 @@ getGen out = do
         need [htmlexe]
         htmlexe `copyFileChanged` out
 
+{-|
+  Call `stack` for the ghc version.
+-}
 getGhcVersion :: GhcVersion -> Action String
 getGhcVersion (GhcVersion _) = trim . fromStdout <$> command [] "stack" ["ghc", "--", "--numeric-version"]
 
+{-|
+  Check `stack.yaml` for the ghcjs compiler version.
+-}
 getGhcjsVersion :: GhcjsVersion -> Action String
 getGhcjsVersion (GhcjsVersion _) = do
         need [cstackyaml]
@@ -123,6 +186,9 @@ getGhcjsVersion (GhcjsVersion _) = do
         Just ghcjs <- parseMonad (.: "compiler") yaml
         pure ghcjs
 
+{-|
+  Combine the GHC and GHCJS versions for the local stack ghcjs directory.
+-}
 getGhcjsFolder :: GhcjsFolder -> Action String
 getGhcjsFolder (GhcjsFolder _) = do
         ghcVer <- askOracle (GhcVersion ())
@@ -132,17 +198,23 @@ getGhcjsFolder (GhcjsFolder _) = do
         parseGhcjs = (<>) <$> string "ghcjs-" <*> fmap (intersperse '.') (digit `endBy1` char '.') <* count 8 digit
         digit = choice . fmap char $ "0123456789"
 
+{-|
+  Call `stack` to find where programs are (locally) installed.
+-}
 localInstallRoot :: Action FilePath
 localInstallRoot = trim . fromStdout <$> command [] "stack" ["path", "--local-install-root"]
 
+{-|
+  Settings; the names should be relatively clear.
+-}
 shakeDir, distDir, buildDir, outputName, outputJS, outputMinJS, cstackyaml, index, ishallopentodayHtml :: FilePath
 shakeDir = ".build"
 buildDir = "dist-js"
 distDir = "dist"
 outputName = "client"
-outputJS = buildDir </> "client" <.> "js"
-outputMinJS = buildDir </> "client" <.> "min" <.> "js"
-cstackyaml = "client" </> "stack" <.> "yaml"
+outputJS = buildDir </> outputName <.> "js"
+outputMinJS = buildDir </> outputName <.> "min" <.> "js"
+cstackyaml = outputName </> "stack" <.> "yaml"
 index = "index" <.> "html"
 ishallopentodayHtml = "ishallopentoday-html" <.> exe
 
